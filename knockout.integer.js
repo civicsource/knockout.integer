d393deb0dc5b0258daf912296458045422e07679
Mark D'Arensbourg <mdarens@gmail.com>
2016-06-27 09:30:29 -0500

    make consumable via npm (#1)
    - add linting

--- knockout.integer.js
+++ knockout.integer.js
@@ -1,37 +1,30 @@
-﻿(function(root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["knockout", "accounting"], factory);
-	} else {
-		// Browser globals
-		factory(ko, accounting);
-	}
-}(this, function (ko, acct) {
-	ko.extenders.integer = function (target, defaultValue) {
-		var result = ko.computed({
-			read: target, //always return the original observable's value
-			write: function (value) {
-				//force to a valid integer
-				var integerValue = parseInt(value, 10);
-				var intDefault = parseInt(defaultValue,10);
-				if (isNaN(integerValue)) {
-					if (!isNaN(intDefault)) {
-						target(intDefault);
-					}
-					else {
-						target(0);
-					}
+﻿var ko = require("knockout");
+var acct = require("accounting");
+
+ko.extenders.integer = function (target, defaultValue) {
+	var result = ko.computed({
+		read: target, //always return the original observable's value
+		write: function (value) {
+			//force to a valid integer
+			var integerValue = parseInt(value, 10);
+			var intDefault = parseInt(defaultValue,10);
+			if (isNaN(integerValue)) {
+				if (!isNaN(intDefault)) {
+					target(intDefault);
 				}
 				else {
-					target(integerValue);
+					target(0);
 				}
 			}
-		});
+			else {
+				target(integerValue);
+			}
+		}
+	});
 
-		result.formatted = ko.computed(function () {
-			return acct.formatNumber(result());
-		});
+	result.formatted = ko.computed(function () {
+		return acct.formatNumber(result());
+	});
 
-		return result;
-	};
-}));
+	return result;
+};
